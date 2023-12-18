d8de79ebd8ad1424c0fe44bfb342d26734d50047
Ruben Vorderman <rubenvorderman@xs4all.nl>
2023-12-18 20:19:54 +0100

    Simplify using a polling model

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,8 @@
  * The code works as follows
  * 1. Each pin is coupled to a midi pitch. 
  * 2. All pins are set to input pullup mode. 
- * 3. All pins trigger an interrupt. 
- * 4. The interrupt routine checks the pin states and if something is changed
- *    sends a midi event. 
- * 5. The main loop puts the microcontroller in sleep mode. It waits for
- *    interrupts and does not use power.
- * 
- * Since the interrupt routine can only run one at a time, there is a 
- * possibility that the latency for multiple keypresses becomes too much. 
- * Given that the wakeup from sleep mode takes about 10 microseconds, this 
- * should not be the case. 
- * 
- * The advantage of this method that only power is used when pins change state. 
- * So pressing down a key for two seconds only uses power for the press and the 
- * letting go. A polling method where each key is checked every x milliseconds, 
- * does use power on every poll.
+ * 3. Pins are polled and MIDI events are sent. 
+ * 4. A 1 ms wait is induced to prevent using too much power.
  */
 
 #include <Arduino.h>
@@ -93,63 +80,49 @@ static inline void sendMidiEvent(uint8_t event, uint8_t channel, uint8_t pitch,
     MidiUSB.sendMIDI(ev);
 }
 
-static volatile uint32_t PIN_STATE_SAVE = 0;
-
-/**
- * @brief check which keys are pressed and send a MIDI event if a state has
- * has changed. 
- * The code is designed to run as interrupt routine. It blocks al incoming 
- * interrupts while running.
- */
-static void updateMidiState(void) {
-    noInterrupts();
-    uint32_t current_pin_states = 0;
-    uint32_t previous_pin_states = PIN_STATE_SAVE;
-    for (uint32_t i=0; i < NUMBER_OF_KEYS; i++) {
-        uint8_t pin = PINS_AND_PITCHES[i].pin;
-        uint8_t pitch = PINS_AND_PITCHES[i].pitch;
-        uint32_t pin_mask = (uint32_t)1 << i;
-        // Input pullup makes pins LOW when the key is pressed.
-        bool current_pin_state = digitalRead(pin) == LOW;
-        bool previous_pin_state = previous_pin_states & pin_mask;
-        if (previous_pin_state != current_pin_state) {
-            if (current_pin_state) {
-                current_pin_states |= pin_mask;
-                sendMidiEvent(MIDI_EVENT_NOTE_ON, MIDI_CHANNEL, pitch, MIDI_VELOCITY);
-            } else {
-                sendMidiEvent(MIDI_EVENT_NOTE_OFF, MIDI_CHANNEL, pitch, MIDI_VELOCITY);
-            }
-        }    
-    }
-    if (current_pin_states) {
-        digitalWrite(LED_BUILTIN, LOW);
-    } else {
-        digitalWrite(LED_BUILTIN, HIGH);
-    }
-    // USB is not realtime. Make sure the event arrives ASAP.
-    MidiUSB.flush();
-    PIN_STATE_SAVE = current_pin_states;
-    interrupts();
-}
-
 /**
  * @brief Set all pins to pullup mode and make sure they can interrupt.
  * 
  */
 void setup() {
-    noInterrupts();
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
     for (size_t i=0; i < NUMBER_OF_KEYS; i++) {
         uint8_t pin = PINS_AND_PITCHES[i].pin;
         pinMode(pin, INPUT_PULLUP);
-        attachInterrupt(digitalPinToInterrupt(pin),updateMidiState, CHANGE);
     }
-    interrupts();
 }
 
 void loop(void) {
+    uint32_t current_pin_states = 0;
     while (true) {
-        __WFI();
+        uint32_t previous_pin_states = current_pin_states;
+        current_pin_states = 0;
+        for (uint32_t i=0; i < NUMBER_OF_KEYS; i++) {
+            uint8_t pin = PINS_AND_PITCHES[i].pin;
+            uint8_t pitch = PINS_AND_PITCHES[i].pitch;
+            uint32_t pin_mask = (uint32_t)1 << i;
+            // Input pullup makes pins LOW when the key is pressed.
+            bool current_pin_state = digitalRead(pin) == LOW;
+            bool previous_pin_state = previous_pin_states & pin_mask;
+            if (previous_pin_state != current_pin_state) {
+                if (current_pin_state) {
+                    current_pin_states |= pin_mask;
+                    sendMidiEvent(MIDI_EVENT_NOTE_ON, MIDI_CHANNEL, pitch, MIDI_VELOCITY);
+                } else {
+                    sendMidiEvent(MIDI_EVENT_NOTE_OFF, MIDI_CHANNEL, pitch, MIDI_VELOCITY);
+                }
+            }    
+        }
+        if (current_pin_states ^ previous_pin_states) {
+            // If there is any difference, flush so events are sent ASAP.
+            MidiUSB.flush();
+        }
+        if (current_pin_states) {
+            digitalWrite(LED_BUILTIN, LOW);
+        } else {
+            digitalWrite(LED_BUILTIN, HIGH);
+        }
+        delay(1);
     }
 }
